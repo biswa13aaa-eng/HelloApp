1dac28f56a780a5671d2a72e66cefd5521f2b9db
biswa13aaa-eng <biswa13aaa@gmail.com>
2026-03-25 08:49:18 +0530

    Display Hello with Multiple Command-Line Arguments using String

--- HelloApp.java
+++ HelloApp.java
@@ -1,27 +1,25 @@
+/**
+ * HelloApp UC 4C:
+ * This program accepts multiple command-line arguments (names)
+ * and displays a personalized greeting using String.join().
+ * If no names are provided, it displays "Hello, World!".
+ *
+ * Usage: java HelloApp [name1] [name2] ... [nameN]
+ */
+
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        // If no command-line arguments are given
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } 
-        else {
-            StringBuilder nameBuilder = new StringBuilder();
+        String names = "World";
 
-            // Add names with comma and space
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            // Remove the last ", "
-            String names = "";
-            if (nameBuilder.length() > 0) {
-                names = nameBuilder.substring(0, nameBuilder.length() - 2);
-            }
-
-            // Print greeting
-            System.out.println("Hello, " + names + "!");
+        // Check if any command-line arguments are provided
+        if (args.length > 0) {
+            // Join all names with comma and space
+            names = String.join(", ", args);
         }
+
+        // Display greeting
+        System.out.println("Hello, " + names + "!");
     }
 }
